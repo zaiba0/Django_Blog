22449032a2fa48c9735c9f74cfe18bafab9830f3
zaiba0 <shahzaib092123@gmail.com>
2025-03-27 23:21:11 +0530

    06-User-Registration-Form

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -32,6 +32,10 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    # Third-party apps
+    'crispy_forms',         # ✅ Add this
+    'crispy_bootstrap4',    # ✅ Add this for Bootstrap 4
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
